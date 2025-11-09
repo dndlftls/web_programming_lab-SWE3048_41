cb810103b8c1acef2aadcb61e0a3c27f8731ff77
dndlftls <bsnmjr4@gmail.com>
2025-11-09 09:59:58 +0000

    PA2

---
+++ Assignment_2/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+app.use("/", express.static(path.join(__dirname, "week4")));
+
+app.listen(app.get("port"), () => {
+    console.log("listening in ", app.get("port"), "port");
+});
+
+module.exports = app;
