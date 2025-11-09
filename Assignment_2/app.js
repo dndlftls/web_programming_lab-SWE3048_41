ae6b2aacf233c4e43005867eafa160e0db4fbe56
dndlftls <bsnmjr4@gmail.com>
2025-11-09 10:29:35 +0000

    Assignment

--- Assignment_2/app.js
+++ Assignment_2/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
-app.use("/", express.static(path.join(__dirname, "week4")));
+app.use("/", express.static(path.join(__dirname, "week_4")));
 
 app.listen(app.get("port"), () => {
     console.log("listening in ", app.get("port"), "port");
